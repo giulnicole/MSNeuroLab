7f6efffbd14a0a606859d6bdf311c5f6d911fef8
giulnicole <gn.baldrighi@gmail.com>
2025-04-29 11:13:30 +0300

    Upd April 2025

--- signing_to_github.R
+++ signing_to_github.R
@@ -1,10 +1,13 @@
 # Configure Github and sign in
 
 # install.packages("usethis")
+# install.packages("devtools")
+# install.packages("roxygen2")
+library(devtools)
+library(roxygen2)
 
 library(usethis)
 use_git_config(user.name="giulnicole", user.email="gn.baldrighi@gmail.com")
 
-library(devtools)
-library(roxygen2)
+
 
