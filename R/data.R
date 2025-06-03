f91926dcbc06984d0de1851a3fd7e78b46f7da31
giulnicole <gn.baldrighi@gmail.com>
2025-06-03 11:03:26 +0300

    documented data june, 3

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example dataset for analysis
+#'
+#' A sample dataset with demographic variables and smoking status of MS patients.
+#'
+#' @format A data frame with 146 observations and n avriables:
+#' \describe{
+#'   \item{age}{Age in years}
+#'   \item{sex}{Sex ("Male", "Female")}
+#'   \item{smoker}{Smoking status ("Yes", "No")}
+#' }
+#'
+#' @usage data(data)
+#' @docType data
+#' @keywords datasets
+"data"
